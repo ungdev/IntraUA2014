f4c01c729df85d92d4c83c11be5e2414d2499e1d
Exalyon <ivanis.kouame@gmail.com>
2014-11-26 02:46:02 +0100

    Little typo

--- migrations/2_create_users.rb
+++ migrations/2_create_users.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     create_table(:users) do
       primary_key :id
       String   :username, :null=>false, :unique=>true
-      String   :token, :text=>true, :null=>false
+      String   :token, :text=>true, :null=>true
       DateTime :last_login
       TrueClass :admin, :null=>false, :default=>false 
     end
