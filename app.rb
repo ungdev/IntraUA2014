1dbd81a443a599b5f118b6f89a1c8659c2971292
Exalyon <ivanis.kouame@gmail.com>
2014-11-26 02:46:10 +0100

    Big update

--- app.rb
+++ app.rb
@@ -2,37 +2,52 @@ require 'sinatra'
 require 'rubygems'
 require 'sequel'
 require 'json'
+require 'digest'
+require 'base64'
 
 require 'dotenv'
 Dotenv.load
-require 'digest'
+
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
 require_relative 'model'
-require 'base64'
+
 disable :raise_errors
 disable :show_exceptions
 
-def helpers
-    def authenticate(username,token,adminRequired)
-        user = DB[:users].first(:username=>loginInformation["username"])
-        halt 401 unless user["token"] == token
-        halt 403 unless adminRequired == false or adminRequired == true and user["admin"] == true
+helpers do
+    def authenticate(adminRequired)
+        halt 400,  {:errors => "Missing parameters"}.to_json if @data["username"].nil? or @data["token"].nil?
+        
+        user = User.first(:username=>@data["username"])
+        
+        halt 401,  {:errors => "User not found"}.to_json if user.nil?
+        halt 401,  {:errors => "Wrong token"}.to_json unless user.token == @data["token"]
+        halt 403,  {:errors => "Unauthorized action"}.to_json unless adminRequired == false or (adminRequired == true and user.admin == true)
+        
+        @data.delete "username"
+        @data.delete "token"
     end
+    
+    def find (model, id)
+    end
+end
+
+before do
+    content_type :json
+    if request.body.size > 0 then @data = JSON.parse request.body.read end
 end
 
 post "/auth" do
     
-    loginInformation = JSON.parse request.body.read
+    halt 400, {:errors => "Missing parameters"}.to_json if @data["username"].nil? or @data["password"].nil?
     
-    halt 400, {:errors => "Missing parameters"}.to_json if loginInformation["username"].nil? or loginInformation["password"].nil?
+    user = User.first(:username=>@data["username"])
     
-    user = DB[:users].first(:username=>loginInformation["username"])
+    halt 401, {:errors => "User not found"}.to_json  if user.nil?
     
-    halt 403, {:errors => "User not found"}.to_json  if (user.nil?)
-    
-    password = loginInformation["password"]
-    salt = user[:salt]
+    password = @data["password"]
+    salt = user.salt
     salted = password + '{' + salt + '}'
     digest = Digest::SHA512.digest(salted)
     for i in (1...5000) do
@@ -40,26 +55,26 @@ post "/auth" do
     end
     encodedPassword = Base64.strict_encode64(digest)
     
-    halt 403,  {:errors => "Wrong password"}.to_json  unless user[:password] == encodedPassword
+    halt 401,  {:errors => "Wrong password"}.to_json  unless user.password == encodedPassword
     
     token = SecureRandom.hex
-    user[:token]= token
+    user.token = token
+    user.last_login = Time.now.getutc
     user.save
     
-    content_type :json
     {:token => user[:token], :admin => user[:admin]}.to_json
     
 end
 
 ['tournaments', 'events', 'challenges'].each do |path|
+    
   get "/#{path}" do
-     content_type :json
      DB[path.to_sym].all.to_json
   end
   
   post "/#{path}" do
-  content_type :json
-         @entity = Object.const_get(path[0...-1].capitalize).new(JSON.parse request.body.read)
+         authenticate(path == "tournament")
+         @entity = Object.const_get(path[0...-1].capitalize).new(@data)
          halt 400, {:errors => @entity.errors}.to_json unless @entity.valid?
          @entity.save
          status 201
@@ -67,32 +82,25 @@ end
 
   get %r{/#{path}/(?<id>\d+)} do |id|
      content_type :json
-     DB[path.to_sym].where(:id=>id.to_i).first!.to_json
+      @entity = DB[path.to_sym].where(:id=>id.to_i).first
+      halt 404,  {:errors => "#{path.capitalize} not found"}.to_json unless not @entity.nil? 
+      @entity.json
   end
   
   put %r{/#{path}/(?<id>\d+)} do |id|
-     content_type :json
-       DB[path.to_sym].first!(:id=>id.to_i).update (JSON.parse request.body.read)
+       authenticate(path == "tournament")
+       halt 404,  {:errors => "#{path.capitalize} not found"}.to_json unless DB[path.to_sym].where(:id=>id.to_i).update (@data) > 0
        status 204
   end
   
   delete %r{/#{path}/(?<id>\d+)} do |id|
-       halt 404 unless DB[path.to_sym].where(:id=>id.to_i).delete > 0
+       authenticate(path == "tournament")
+       halt 404,  {:errors => "#{path.capitalize} not found"}.to_json unless DB[path.to_sym].where(:id=>id.to_i).delete > 0
        status 204
   end
 end
 
 error JSON::ParserError do
-    raise 400, "Request body is not a correct JSON"
+    status 400
+    {:errors => "Request body is not a correct JSON" }.to_json
 end
-
-error Sequel::NoMatchingRow do
-    raise 404
-end
-
-error 404 do
-   {:errors => "Resource not found" }.to_json
-end
-
-
-
