0290d4a9c3682639ea34a7c285823c93a2d42b4b
Extaze <gabriel.juchault@gmail.com>
2014-11-30 15:28:27 +0100

    Events progressing

--- static/js/admin.events.js
+++ static/js/admin.events.js
@@ -13,4 +13,47 @@
         useSeconds: false,
         useStrict: true
     });
+
+    $.ajax({
+        type: 'get',
+        url: '/events',
+        success: function (msg) {
+            render(msg);
+        }
+    });
+
+    function render (events) {
+        var $list = $('#list');
+        Object.keys(events).forEach(function (event) {
+            var $li = $('<li/>');
+            var $a = $('<a/>').attr('href').text('Supprimer');
+            $li.text(events[event].name);
+            $li.append($a);
+            $list.append($li);
+        });
+    }
+    render({});
+
+    var $title = $('#title');
+    var $date = $('#date');
+    var $desc = $('#description');
+    $('#eventForm').off('submit').submit(function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            type: 'post',
+            url: '/events',
+            data: JSON.stringify({
+                title: $title.val(),
+                description: $desc.val(),
+                date: parseInt(moment($date.val(), 'DD/MM/YYYY HH:mm').format('YYYY/MM/DD HH:mm'), 10)
+            }),
+            success: function (msg) {
+                console.log(msg);
+            },
+            error: function (msg) {
+                console.log(msg);
+            }
+        });
+    });
 }());
--- static/js/events.js
+++ static/js/events.js
@@ -3,32 +3,35 @@
 
 (function () {
     'use strict';
-    var events = {
-        event1: {
-            title: 'Event 1',
-            date: 1415246954059,
-            description: 'Super event suuuuper event'
+    $.ajax({
+        type: 'get',
+        url: '/events',
+        success: function (msg) {
+            render(msg);
         }
-    };
+    });
 
     // Rendering
-    var $list = $('#list');
-    Object.keys(events).forEach(function (name) {
-        var $h3 = $('<h3/>').text(events[name].title);
-        var $clock = $('<div/>').addClass('clock');
-        var $descritpion = $('<div/>')
-                                .text(events[name].description)
-                                .addClass('description');
+    function render (events) {
+        var $list = $('#list');
+        Object.keys(events).forEach(function (name) {
+            var $h3 = $('<h3/>').text(events[name].title);
+            var $clock = $('<div/>').addClass('clock');
+            var $descritpion = $('<div/>')
+                                    .text(events[name].description)
+                                    .addClass('description');
 
-        // Countdown
-        var clock = new FlipClock($clock, {
-            autoStart: false,
-            countdown: true
+            // Countdown
+            var clock = new FlipClock($clock, {
+                autoStart: false,
+                countdown: true
+            });
+            var thisDate = new Date().getTime();
+            clock.setTime((events[name].date - thisDate) / 1000);
+            clock.start();
+
+            $list.append($h3).append($clock).append($descritpion);
         });
-        var thisDate = new Date().getTime();
-        clock.setTime((events[name].date - thisDate) / 1000);
-        clock.start();
-
-        $list.append($h3).append($clock).append($descritpion);
-    });
+    }
+    render({});
 }());
