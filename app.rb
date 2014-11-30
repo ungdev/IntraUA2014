9f052335788f074feaf4d1bfa1bfd9cb29d43aeb
Exalyon <ivanis.kouame@gmail.com>
2014-11-30 01:29:04 +0100

    typo

--- app.rb
+++ app.rb
@@ -255,7 +255,7 @@ end
 
 get '/index.html' do
     content_type :html
-    autentitcate
+    authenticate
     redirect '/login.html' unless not @user.nil?
     if @user.admin 
         send_file File.expand_path('../static/index.admin.html',  __FILE__) 
